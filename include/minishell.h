8be87b70c281c5ec65c8aa48d4cd01d245d135d2
knusperpeter <christianiaigner@gmail.com>
2024-01-28 23:21:01 +0100

    Remove main.c file

---
+++ include/minishell.h
@@ -0,0 +1,18 @@
+#ifndef		MINISHELL_H
+# define	MINISHELL_H
+
+# include "libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <signal.h>
+# include <limits.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/wait.h>
+
+
+#endif
