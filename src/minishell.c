710b8b861c490cf93dd96dbfe33503d001de38a0
Knusperpeter <chrisaigner0@gmail.com>
2024-02-14 14:18:31 +0100

    fixed prompt, found error in export if HOME is unset

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: chris <chris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 20:17:44 by chris             #+#    #+#             */
-/*   Updated: 2024/02/13 16:15:19 by chris            ###   ########.fr       */
+/*   Updated: 2024/02/14 14:03:25 by chris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*prompt(void)
 {
 	char	*line;
 
-	line = readline("minishell🔮: 🚬🦦 ✗"); // check rl_redisplay
+	line = readline("minishell🔮: 🚬🦦❯ "); // check rl_redisplay
 	if (ft_strlen(line) > 0)
 		add_history(line);
 	return (line);
@@ -38,10 +38,25 @@ void	init_loop_data(t_common *c){
 	c->raw_prompt = NULL;
 	c->prompt = NULL;
 }
+
+int	check_cmd(char *cmd, t_node *tokenslist)
+{
+	int		i;
+	int		size;
+
+	i = 0;
+	size = ft_strlen(tokenslist->str[0]);
+	while (cmd[i] && cmd[i] != ' ')
+		i++;
+	if (ft_strncmp(cmd, tokenslist->str[0], size) || i != size)
+		return (0);
+	return (1);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	t_common	c;
-	int			size;//Validate size before allocation. The size variable
+	//Validate size before allocation. The size variable
 	// should be validated or bounded checked before allocating memory to avoid potential integer overflows.
 	(void)		ac;
 	(void)		av;
@@ -59,20 +74,19 @@ int	main(int ac, char **av, char **envp)
 			c.tokenslist->str = ft_split(c.raw_prompt, ' ');
 			if (!c.tokenslist->str)
 				return (perror("Error initializing tokenslist\n"), 1);
-			size = ft_strlen(c.tokenslist->str[0]);
-			if (!ft_strncmp("pwd", c.tokenslist->str[0], size))
+			if (check_cmd("pwd", c.tokenslist))
 				ft_pwd();
-			if (!ft_strncmp("export", c.tokenslist->str[0], size))	
+			if (check_cmd("export", c.tokenslist))	
 				ft_export(c.tokenslist->str, c.env);
-			if (!ft_strncmp("env", c.tokenslist->str[0], size))
+			if (check_cmd("env", c.tokenslist))
 				ft_env(c.env);
-			if (!ft_strncmp("exit", c.tokenslist->str[0], size))
+			if (check_cmd("exit", c.tokenslist))
 				ft_exit(&c, c.tokenslist->str);
-			if (!ft_strncmp("unset", c.tokenslist->str[0], size))
+			if (check_cmd("unset", c.tokenslist))
 				ft_unset(c.tokenslist->str, &c);
-			if (!ft_strncmp("echo", c.tokenslist->str[0], size))
+			if (check_cmd("echo", c.tokenslist))
 				ft_echo(c.tokenslist->str);
-			if (!ft_strncmp("cd", c.tokenslist->str[0], size))
+			if (check_cmd("cd", c.tokenslist))
 				ft_cd(c.tokenslist->str, &c);
 			free_2d(c.tokenslist->str);
 			free(c.raw_prompt);
