046a9ad4a0ba36aed87e5cc8fdfff08820da1f5b
Michael Heider <145189602+HeiMichael@users.noreply.github.com>
2024-02-14 13:35:05 +0100

    Lexer (#14)
    * Added lexer.c
    
    * added .gitignore
    
    * lexer input handling fixed, handling quotes not entirely
    
    * lexer done for (heavy) testing
    
    * lexer before heavy testing
    
    * lexer before heavy testing
    
    * test commit from laptop, removed some comments from lexer.c
    
    * shortened the lexer function
    
    * started second part of lexer. can handle basich input now (except for input starting with space); still to do: handle seperators and handle quouts
    
    * rewrite lexer in file lexer_new.c / almost done
    
    ---------
    
    Co-authored-by: Michael Heider <miheider@c3r8p3.42vienna.com>
    Co-authored-by: knusperpeter <121819658+knusperpeter@users.noreply.github.com>
    Co-authored-by: miheider <miheider@student.42vienna.com>

---
+++ src/lexer_neu.c
@@ -0,0 +1,152 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miheider <miheider@42>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 17:16:46 by miheider          #+#    #+#             */
+/*   Updated: 2024/02/06 21:14:34 by miheider         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "minishell.h"
+#include "../include/minishell.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	unsigned int	x;
+	unsigned int	y;
+
+	x = 0;
+	while (src[x] != '\0')
+		x++;
+	if (size == 0)
+		return (x);
+	y = 0;
+	while (src[y] != '\0' && y < (size -1))
+	{
+		dst[y] = src[y];
+		y++;
+	}
+	dst[y] = '\0';
+	return (x);
+}
+
+int check_char(char *character)
+{
+    char special[11] = "*?[]()<>|#\"";
+    int i = 0;
+
+    if (*character == '.' || *character == '\'' || *character == '\"')
+        return (2);
+
+    while (i < 11)
+    {
+        if (special[i] == *character)
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+void set_up_array(int wc, int cc, char *input)
+{
+    char *new_string;
+    int i = 0;
+    int j = 0;
+
+    new_string = (char *)malloc(sizeof(char) * (wc + cc));
+    if (!new_string)
+    {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    while (input[i] == ' ')
+        i++;
+    while (input[i])
+    {
+        while (input[i] && input[i] != ' ')
+        {
+            new_string[j] = input[i];
+            i++;
+            j++;        
+        }
+        while (input[i] == ' ')
+            i++;
+    }
+    new_string[j] = '\0';
+    printf("erg:___%s___\n", new_string);
+    free(new_string);
+}
+
+
+void prep_input(char *input)
+{
+    int wc = 0;
+    int cc = 0;
+    char *string;
+
+    string = input;
+    if (check_char(input) == 1)
+        wc--;
+    while (*input)
+    {
+        while (*input == ' ')
+            input++;
+        if (*input != ' ')
+        {
+            if (*input == '\0')
+                break;
+            wc++;
+        }
+        while (*input && *input != ' ')
+        {
+            if (check_char(input) == 1 && ((*(input - 1) == ' ') || (*(input - 1) == *input)))
+                cc++;
+            else if (check_char(input) == 1 && *(input - 1) != ' ')
+            {
+                wc++;
+                cc++;
+            }
+            else if (check_char(input) == 0 && (check_char(input - 1) == 1 || check_char(input - 1) == 2))
+            {
+                wc++;
+                cc++;
+            }
+            else if (check_char(input) == 0 && (check_char(input - 1) != 1)) 
+                cc++;
+            else
+            {
+                if (*(input - 1) != ' ')
+                {
+                    wc++;
+                    cc++;
+                }
+                else 
+                {
+                    cc++;
+                }
+                input++;
+                while (check_char(input) != 2 && *input)
+                {
+                    cc++;
+                    input++;
+                }
+                cc++;
+            }
+            input++;
+        }
+    }
+    printf("wc: %d\n", wc);
+    printf("cc: %d\n", cc);
+    set_up_array(wc, cc, string);
+}
+
+int  main(int argc, char **argv)
+{
+    argc = argc - 1 + 1;
+    prep_input(argv[1]);
+    return (0);
+}
