dcc47e51d840d7672632b2a5862efcb18fb272e3
knusperpeter <121819658+knusperpeter@users.noreply.github.com>
2024-02-28 14:48:36 +0100

    Executor (#21)
    * Refactor code: Move open_io function and update dependencies
    
    * Fix file I/O error handling and implement check_io_name function
    
    * Fix file descriptor handling and implement pipe creation
    
    * Update code in src/exec.c, src/minishell.c, and src/parsing_cmd_table.c
    
    ---------
    
    Co-authored-by: knusperpeter <christianiaigner@gmail.com>

--- src/exec.c
+++ src/exec.c
@@ -6,11 +6,12 @@
 /*   By: caigner <caigner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 20:25:50 by chris             #+#    #+#             */
-/*   Updated: 2024/02/23 16:45:32 by caigner          ###   ########.fr       */
+/*   Updated: 2024/02/28 14:27:55 by caigner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <stdlib.h>
 /*
 if i only have one cmd, do i still need to fork? 
 	->probably yes, so after execve i can free everything, and in case of pipes to close them.
@@ -18,16 +19,6 @@ if i only have one cmd, do i still need to fork?
 if no output redirect, and only 1 cmd, i need no pipe, right?
 if 
 */
- 
-char	*prompt(void)
-{
-	char	*line;
-
-	line = readline("minishell🔮: 🚬🦦❯ "); // check rl_redisplay
-	if (ft_strlen(line) > 0)
-		add_history(line);
-	return (line);
-}
 
 int	check_cmd(char *cmd, t_cmd_table *cmd_struct)
 {
@@ -41,12 +32,6 @@ int	check_cmd(char *cmd, t_cmd_table *cmd_struct)
 	if (ft_strncmp(cmd, cmd_struct->str[0], size) || i != size)
 		return (0);
 	return (1);
-}
-
-void	init_loop_data(t_common *c){
-	c->raw_prompt = NULL;
-	c->tokens = NULL;
-	c->cmd_struct = NULL;
 }
 
 int	ft_execute_builtins(t_cmd_table *cmd, t_common *c)
@@ -73,17 +58,73 @@ int	ft_execute_builtins(t_cmd_table *cmd, t_common *c)
 	return (EXIT_SUCCESS);
 }
 
-// ex.: < in cat | cat > out
-int	ft_loop(t_common *c)
+/* //check if it is a directory
+int	check_if_dir(t_io_red *io)
 {
-	init_loop_data(c);
-	c->raw_prompt = prompt();
-	if (c->raw_prompt[0])
+	struct stat	s;
+
+	if (io->type == REDIR_IN || io->type == HEREDOC)
 	{
-		//lexing&parsing
-		ft_parsing(c);
-//		ft_execute(c);
-//		ft_execute_builtins(c->cmd_struct->content, c);
+		if (stat(io->infile, &s) == 0)
+		{
+			if (S_ISDIR(s.st_mode))//check if this is allowed
+				return (1);
+			else
+				return (0);
+		}
 	}
+	else if (io->type == REDIR_OUT || io->type == APPEND)
+	{
+		if (stat(io->outfile, &s) == 0)
+		{
+			if (S_ISDIR(s.st_mode))//check if this is allowed
+				return (1);
+			else
+				return (0);
+		}
+	}
+	else
+		return (-1);
+} */
+
+int	pipe_needed(t_cmd_table *cmd)
+{
+	if (cmd->read_fd != 0 || cmd->write_fd != 1)
+		return (1);
 	return (0);
 }
+
+int	create_pipe(t_pipe *new)
+{
+	if (new->read_fd != -1 || new->write_fd != -1)
+		printf("Pipe not empty");
+	if (pipe(new->pipes) == -1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+// wenn mehrere infiles, muss es das letzte sein. aber es muss erst gecheckt werden ob beide exisiteren.
+int	ft_execute(t_common *c)
+{
+	t_list		*curr_cmd;
+	t_cmd_table	*curr_cmd_table;
+
+	curr_cmd = c->cmd_struct;
+	while (curr_cmd)
+	{
+		curr_cmd_table = curr_cmd->content;
+		open_io(curr_cmd_table->io_red, curr_cmd_table);
+		curr_cmd = curr_cmd->next;
+	}
+	curr_cmd = c->cmd_struct;
+	while (curr_cmd)
+	{
+		if (pipe_needed(curr_cmd->content))
+			create_pipe(&c->new_pipe);
+		
+		curr_cmd = curr_cmd->next;
+	}
+	return (EXIT_SUCCESS);
+}
+
+// ex.: < in cat | cat > out
---
+++ src/open_io.c
@@ -0,0 +1,143 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   open_io.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caigner <caigner@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 13:19:00 by caigner           #+#    #+#             */
+/*   Updated: 2024/02/26 16:24:54 by caigner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void	ft_printerrno(char *s)
+{
+	ft_putstr_fd("minishell: ", 2);
+	if (s)
+		ft_putstr_fd(s, 2);
+	ft_putstr_fd(strerror(errno), 2);
+}
+
+void	here_doc(char *limiter, t_cmd_table *cmd_table)
+{
+	char		*buf;
+
+	if (cmd_table->read_fd == -1)
+		ft_printerrno(limiter);
+	while (1)
+	{
+		write(1, "minishell heredoc> ", 15);
+		if (get_next_line(0, &buf, 0) < 0)
+			exit (1);
+		if (buf == NULL || *buf == '\0')
+			break ;
+		if (ft_strncmp(limiter, buf, ft_strlen(limiter)) == 0)
+			break ;
+		write(cmd_table->read_fd, buf, ft_strlen(buf));
+	}
+	get_next_line(0, &buf, 1);
+	free(buf);
+	close(cmd_table->read_fd);
+	cmd_table->read_fd = open(cmd_table->heredoc_name, O_RDONLY);
+	if (cmd_table->read_fd == -1)
+	{
+		ft_printerrno(NULL);
+		unlink(cmd_table->heredoc_name);
+	}
+}
+
+int	open_infile(t_io_red *io, t_cmd_table *cmd_node)
+{
+	if (io->type == HEREDOC)
+		{
+			cmd_node->read_fd = open(cmd_node->heredoc_name, O_CREAT | O_WRONLY
+					| O_TRUNC, 0644);
+			here_doc(io->heredoc_limiter, cmd_node);
+		}
+	else
+		cmd_node->read_fd = open(io->infile, O_RDONLY);
+	if (cmd_node->read_fd == -1)
+		return (ft_printerrno(io->infile), EXIT_FAILURE);
+}
+
+//ACHTUNG: ich kann erst öffnen, wenn expanded wurde!!!
+int	open_file(t_io_red *io, t_cmd_table *cmd_node)
+{
+	if (io->type == HEREDOC || io->type == REDIR_IN)
+		return (open_infile(io, cmd_node));
+	else if (io->type == REDIR_OUT || io->type == APPEND)
+	{
+		if (io->type == REDIR_OUT)
+			cmd_node->write_fd = open(io->outfile, O_WRONLY | O_TRUNC
+					| O_CREAT, S_IRUSR | S_IWUSR);
+		else
+			cmd_node->write_fd = open(io->outfile, O_WRONLY | O_APPEND
+					| O_CREAT, S_IRUSR | S_IWUSR);
+		if (cmd_node->write_fd == -1)
+			return (ft_printerrno(io->outfile), EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+void	ft_close_old_fd(t_cmd_table *cmd_node, t_io_red *io)
+{
+	if ((io->type == HEREDOC || io->type == REDIR_IN) && cmd_node->read_fd != 0)
+		close(cmd_node->read_fd);
+	else if ((io->type == REDIR_OUT || io->type == APPEND)
+			&& cmd_node->write_fd != 1)
+		close(cmd_node->write_fd);	
+}
+
+int	open_io(t_list *io, t_cmd_table *cmd_node)
+{
+	t_list		*tmp;
+	int			status;
+
+	status = EXIT_SUCCESS;
+	tmp = io;
+	while (tmp)
+	{
+		ft_close_old_fd(cmd_node, tmp->content);
+		if (open_file(tmp->content, cmd_node) == EXIT_FAILURE)
+			status = EXIT_FAILURE;
+//if multiplel infiles -> just take the last one. This should happen already in open_file
+		tmp = tmp->next;
+	}
+	if (status == EXIT_FAILURE && cmd_node->heredoc_name)
+		unlink(cmd_node->heredoc_name);
+	return (status);
+}
+
+/* Restoration function, if needed:
+void	open_file(t_io_red *io, t_cmd_table *cmd_node)
+{
+	if (io->type == HEREDOC)
+	{
+		cmd_node->read_fd = open(cmd_node->heredoc_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		here_doc(io->heredoc_limiter, cmd_node);
+		
+	}
+	else if (io->type == REDIR_IN)
+	{
+		cmd_node->read_fd = open(io->infile, O_RDONLY);
+
+	}
+	else if (io->type == REDIR_OUT)
+	{
+		cmd_node->write_fd = open(io->outfile, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR
+				| S_IWUSR);
+	}
+	else if (io->type == APPEND)
+	{
+		cmd_node->write_fd = open(io->outfile, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR
+				| S_IWUSR);
+	}
+	if (cmd_node->write_fd == -1)
+		printf("minishell: %s: %s\n", io->outfile, strerror(errno));
+} */
