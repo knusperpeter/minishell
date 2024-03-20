bcb582b5b1b14e5f2f7409bc62ff34e2ba353b91
Knusperpeter <chrisaigner0@gmail.com>
2024-03-20 12:23:29 +0000

    Fixed compilation error in lexer_new.c

--- src/lexer_new.c
+++ src/lexer_new.c
@@ -227,7 +227,7 @@ int	no_space_array(int *i, int *j, char *input, char *new_string)
 				return (-10);
 		}
 	}
-	if ((i > 0 && check_char(&input[*i]) == 0
+	if ((*i > 0 && check_char(&input[*i]) == 0
 			&& check_char(&input[*(i) - 1]) == 1)
 		|| (check_char(&input[*i]) == 1
 			&& check_char(&input[*(i) - 1]) == 0
@@ -386,7 +386,7 @@ void	error_check_pipes(int *i, int *pipe, char *input)
 		error_lexer("|", 2);
 	while (input[*i] && input[*i] == ' ')
 	{
-		if (input[*i] == ' ' && input[*(i + 1)] == '\0')
+		if (input[*i] == ' ' && input[*(i) + 1] == '\0')
 			error_lexer("|", 2);
 		(*i)++;
 	}
