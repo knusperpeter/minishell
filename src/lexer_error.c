898fea778242e52e80b1b250dae834d9f1ad0d73
Michael Heider <miheider@c3r7p4.42vienna.com>
2024-05-01 14:04:44 +0200

    test

--- src/lexer_error.c
+++ src/lexer_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caigner <caigner@student.42.fr>            +#+  +:+       +#+        */
+/*   By: miheider <miheider@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:16:46 by miheider          #+#    #+#             */
-/*   Updated: 2024/04/22 17:34:53 by caigner          ###   ########.fr       */
+/*   Updated: 2024/04/27 13:56:42 by miheider         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	error_lexer(char *s, int i)
 {
-	if (*s == '>' || *s == '<' || *s == '|')
+/*	if (*s == '>' || *s == '<' || *s == '|')
 	{
 		if (*s == '<' && i == 3)
-			printf("❌ minishell: syntax error near unexpected token `<'\n");
+			ft_putstr_fd("❌ minishell: syntax error near unexpected token `<'\n", 2);
 		else if (*s == '<' && i > 3)
-			printf("❌ minishell: syntax error near unexpected token `<<'\n");
+			ft_putstr_fd("❌ minishell: syntax error near unexpected token `<<'\n", 2);
 		else if (*s == '>' && i == 3)
-			printf("❌ minishell: syntax error near unexpected token `>'\n");
+			ft_putstr_fd("❌ minishell: syntax error near unexpected token `>'\n", 2);
 		else if (*s == '>' && i > 3)
-			printf("❌ minishell: syntax error near unexpected token `>>'\n");
-		else if (*s == '|' && (i == 2 || i == 3))
-			printf ("❌ minishell: syntax error near unexpected token `|'\n");
+			ft_putstr_fd("❌ minishell: syntax error near unexpected token `>>'\n", 2);*/
+		if (*s == '|' && (i == 2 || i == 3))
+			ft_putstr_fd("❌ minishell: syntax error near unexpected token `|'\n", 2);
 		else if (*s == '|' && i > 3)
-			printf ("❌ minishell: syntax error near unexpected token `||'\n");
+			ft_putstr_fd("❌ minishell: syntax error near unexpected token `||'\n", 2);
 		exit (2);
-	}
+//	}
 }
